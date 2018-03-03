237282236c868ae21a94b48d52d18288dbfa070a
Carol Greene <carolgreene3@gmail.com>
2018-03-03 17:56:09 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,7 +2,7 @@ CREATE TABLE bears (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER,
-  gender CHAR(1)(The choices would be "M" or "F"),
+  gender BLOB,
   color TEXT,
   temperment TEXT,
   alive NUMERIC
